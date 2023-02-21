8c49ca487a3488fced7d2a7b4acff3af1c749ea5
Dominik Szczęśniak <dominikszczesniak128@gmail.com>
2023-02-21 15:43:03 +0100

    removed 3 methods

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -13,16 +13,4 @@ public class Application {
 	static void printName(String name) {
 		System.out.println(name);
 	}
-
-	static void movement() {
-		System.out.println("movement");
-	}
-
-	static void feature() {
-		System.out.println("feature");
-	}
-
-	static void addition() {
-		System.out.println("addition");
-	}
 }
